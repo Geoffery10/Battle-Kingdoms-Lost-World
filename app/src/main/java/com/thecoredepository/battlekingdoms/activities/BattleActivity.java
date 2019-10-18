f965ac0b3db62bcdf91f2c5a8f142c0c674cb400
Geoffery Powell <powellgeoffery@gmail.com>
2019-10-18 12:01:51 -0500

    Added Defualt Code for Travel Map and Battle Activity

--- app/src/main/java/com/thecoredepository/battlekingdoms/activities/BattleActivity.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/activities/BattleActivity.java
@@ -1,6 +1,28 @@
 package com.thecoredepository.battlekingdoms.activities;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BattleActivity extends AppCompatActivity {
+import com.thecoredepository.battlekingdoms.R;
+
+public class BattleActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_party);
+        fullscreen();
+
+
+    }
+
+    private void fullscreen() {
+        View overlay = findViewById(R.id.activity_choose_party);
+
+        overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
