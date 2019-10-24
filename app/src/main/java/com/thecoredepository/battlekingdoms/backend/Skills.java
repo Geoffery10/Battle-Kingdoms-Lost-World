b45715da1fa94af3a8a420f8da973668793608a4
lealmario114 <mario.leal@siu.edu>
2019-10-24 14:19:04 -0500

    skills.java created
    created skill class

---
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Skills.java
@@ -0,0 +1,26 @@
+
+public class Skills {
+
+	String name;
+	String effect;
+	double effectNumber;
+	
+	public Skills() {
+		name = "null";
+		effect = "null";
+		effectNumber = 0;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEffect() {
+		return effect;
+	}
+
+	public double getEffectNumber() {
+		return effectNumber;
+	}	
+	
+}
