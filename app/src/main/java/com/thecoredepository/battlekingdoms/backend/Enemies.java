ca0d9fcae2913dd56866ae31c8d33ed9b2422d64
lealmario114 <mario.leal@siu.edu>
2019-10-24 14:18:15 -0500

    Enemies.java created
    class for enemies
    added some comments

--- app/src/main/java/com/thecoredepository/battlekingdoms/backend/Enemies.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Enemies.java
@@ -2,6 +2,7 @@
 public class Enemies extends Entity{
 
 	//basic constructor that calls on the super class
+	//hass all the same variables except magic 
 	public Enemies(String name, String race, int health, double attack, double defence, double speed) {
 		super();
 		this.name = name;
