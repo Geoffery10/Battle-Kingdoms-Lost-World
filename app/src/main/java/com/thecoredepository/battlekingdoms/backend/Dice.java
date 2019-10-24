bbff8a7a757bc0fb1a45dffd4872c9aae7c5feaa
lealmario114 <mario.leal@siu.edu>
2019-10-24 14:13:50 -0500

    created dice class
    added constructor and getters/setters.
    one of the getters returns the value of however many dice where specified

--- app/src/main/java/com/thecoredepository/battlekingdoms/backend/Dice.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Dice.java
@@ -1,13 +1,45 @@
 package com.thecoredepository.battlekingdoms.backend;
 
+import java.util.Random;
+
 //This class is designed to manage Dice rolls and apply entity modifiers
 
 public class Dice
 {
     //Attributes of Dice
     //2d8 = (numOfDice=2)d(valueOfDice=8)
-    int numOfDice;
+    Random rand =new Random();
+	int numOfDice;
     int valueOfDice;
-    double multiplier; //i.e. 1.5x
+   // double multiplier; //i.e. 1.5x
     double result; //i.e. Attack damage after roll and modifier
+
+    public Dice(int numOfDice, int valueOfDice) {
+    	this.numOfDice = numOfDice;
+    	this.valueOfDice = valueOfDice;
+    	result = 0;
+    }
+    
+    //getters
+    public int getNumOfDice() {
+		return numOfDice;
+	}
+
+	public int getValueOfDice() {
+		return valueOfDice;
+	}
+    
+	//getter for result of dice. uses while loop to "roll" each dice
+	public double getResult() {
+		int x = 0;
+		result = 0;
+		
+		while (x<numOfDice) {
+			int randVal = rand.nextInt(valueOfDice);
+		    result += randVal+1;
+		    x++;
+		}
+		return result;
+	}
+    
 }
