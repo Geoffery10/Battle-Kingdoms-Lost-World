9a384c0a1e29162725af74317b1acb31b1513f6c
lealmario114 <mario.leal@siu.edu>
2019-10-24 14:17:37 -0500

    Update Character.java
    added some comments

--- app/src/main/java/com/thecoredepository/battlekingdoms/backend/Character.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Character.java
@@ -3,6 +3,7 @@ public class Character extends Entity{
 
 	//skill list needed to be added. probably an object array.
 	
+	//all the same variables as Entity for now
 	public Character(String name, String race, int health, int magic, double attack, double defence, double speed) {
 		super();
 		this.name = name;
