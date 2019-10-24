379c77d97a2fd8e78da2e698ffc4d708234f17f7
lealmario114 <mario.leal@siu.edu>
2019-10-24 14:21:15 -0500

    Update Skills.java
    added some comments

--- app/src/main/java/com/thecoredepository/battlekingdoms/backend/Skills.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Skills.java
@@ -1,16 +1,23 @@
 
 public class Skills {
 
+	//base parent class for skills
+	//has three variables for name, effect, and effect number
+
+	//effect number will be related to any stat effects that the 
+	//effect may have
 	String name;
 	String effect;
 	double effectNumber;
 	
+	//constructor
 	public Skills() {
 		name = "null";
 		effect = "null";
 		effectNumber = 0;
 	}
 	
+	//getters
 	public String getName() {
 		return name;
 	}
