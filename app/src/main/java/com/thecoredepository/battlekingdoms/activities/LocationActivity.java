6a43f09b31a7467780f2566c26484f6e04764b77
Geoffery Powell <powellgeoffery@gmail.com>
2019-10-28 14:40:56 -0500

    Started Location Layout

--- app/src/main/java/com/thecoredepository/battlekingdoms/activities/LocationActivity.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/activities/LocationActivity.java
@@ -1,4 +1,45 @@
 package com.thecoredepository.battlekingdoms.activities;
 
-public class LocationActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.thecoredepository.battlekingdoms.R;
+
+public class LocationActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        fullscreen();
+
+        //View Elements
+        Button btnBackHome = findViewById(R.id.btnBackHome);
+
+        //Buttons
+        btnBackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Open Settings Menu
+                //Open Intent
+                //Intent in = new Intent(getApplicationContext(), MainActivity.class);
+                //startActivity(in);
+
+                //Close Settings
+                finish();
+            }
+        });
+    }
+
+    private void fullscreen() {
+        View overlay = findViewById(R.id.activity_travel_map);
+
+        overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
