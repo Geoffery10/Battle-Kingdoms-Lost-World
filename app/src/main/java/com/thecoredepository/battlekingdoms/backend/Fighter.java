80b79c1d39a0ef4736715a4a16dc5fbb632a84a5
lealmario114 <mario.leal@siu.edu>
2019-10-30 21:52:42 -0500

    Update Fighter.java
    added comments

--- app/src/main/java/com/thecoredepository/battlekingdoms/backend/Fighter.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Fighter.java
@@ -1,10 +1,11 @@
-
+//i will add more stuff like a skill list 
+//DONT JUDGE FRANK ON HOW BARE HE IS
 public class Fighter extends Character {
 
 	public Fighter() {
 		super("Frank","Fighter","Human",30,10,2.0,5.0,2);
 	}
-	
+	//this was just to test
 	void printStats() {
 		System.out.println(get_name());
 		System.out.println(get_race());
