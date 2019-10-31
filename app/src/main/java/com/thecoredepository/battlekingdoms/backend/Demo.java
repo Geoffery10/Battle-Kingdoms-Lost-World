3035ae43f215136b48767fb2b4b6ef5c028650a5
lealmario114 <mario.leal@siu.edu>
2019-10-30 21:44:52 -0500

    Demo= main class
    this is the actual main class

---
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Demo.java
@@ -0,0 +1,19 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		//test for dice
+		//Dice demo = new Dice(2,4);
+		
+		//test for demo
+		//Entity demo = new Entity();
+		
+		//test for fighter
+		Fighter demo = new Fighter();
+		
+		//demo.printStat();
+		System.out.println(demo.get_name());
+		System.out.println(":P");
+	}
+
+}
