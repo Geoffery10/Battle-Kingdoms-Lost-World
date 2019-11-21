6f89e8250691915294fae86de9c01fce110161bb
Geoffery Powell <powellgeoffery@gmail.com>
2019-11-20 20:30:34 -0600

    Corrected the rest of the printStatsToConsole
    Co-Authored-By: lealmario114 <lealmario114@users.noreply.github.com>

--- app/src/main/java/com/thecoredepository/battlekingdoms/backend/Knight.java
+++ app/src/main/java/com/thecoredepository/battlekingdoms/backend/Knight.java
@@ -6,17 +6,16 @@ public class Knight extends Character {
 		super("Nellson","Knight","Dragonborn",50,5,5.0,10.0,1);
 	}
 	//this was just to test
-	void printStats() {
-		System.out.println(get_name());
-		System.out.println(get_race());
-		System.out.println(get_type());
-		System.out.println(get_health());
-		System.out.println(get_magic());
-		System.out.println(get_attack());
-		System.out.println(get_defense());
-		System.out.println(get_speed());
-		System.out.println();
-		
+	void printStatsToConsole() {
+		//This function is only for testing
+		System.out.println(getName() + "\n");
+		System.out.println(getCharClass() + "\n");
+		System.out.println(getRace() + "\n");
+		System.out.println(getHealth() + "\n");
+		System.out.println(getMagic() + "\n");
+		System.out.println(getAttack() + "\n");
+		System.out.println(getDefence() + "\n");
+		System.out.println(getSpeed() + "\n");
 	}
 	
 }
